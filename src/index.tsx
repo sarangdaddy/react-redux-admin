a402555f8f97793f29553f9f8546371167294824
sarang_daddy <sarang_daddy@naver.com>
2023-10-25 21:39:33 +0900

    ADD: Added Tanstack-query for data caching

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './styles/theme';
 import GlobalStyle from './styles/GlobalStyle';
@@ -12,6 +13,8 @@ const store = configureStore({
   reducer: rootReducer,
 });
 
+const queryClient = new QueryClient();
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
@@ -19,10 +22,12 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <QueryClientProvider client={queryClient}>
+        <GlobalStyle />
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </QueryClientProvider>
     </ThemeProvider>
   </React.StrictMode>,
 );
