1ead923fc19449bffaf089418d4693eba0f46da3
sarang_daddy <sarang_daddy@naver.com>
2023-10-29 01:18:17 +0900

    FEAT: Created AddUserForm component

---
+++ src/components/AddUserForm/index.tsx
@@ -0,0 +1,136 @@
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useForm } from 'react-hook-form';
+import { Controller } from 'react-hook-form';
+import { addUserData } from '@/apis';
+import CustomCalendar from '../CustomCalendar';
+import SubmitButton from '../SubmitButton';
+import { formatDateToString } from '@/utils/formatDateToString';
+import { SUBMIT_BUTTON } from '@/constants/buttonTitle';
+import { ADD_USER_FORM, PROFILE_LIST, SEX_LIST } from '@/constants/label';
+import { IUser, IRootState } from '@/modules/types';
+import * as S from './styles';
+
+interface IAddUserFormProps {
+  onClose: () => void;
+}
+
+const AddUserForm = ({ onClose }: IAddUserFormProps) => {
+  const dispatch = useDispatch();
+  const users = useSelector((state: IRootState) => state.users.users);
+  const lastId = users[users.length - 1]?.id || 0;
+  const { register, handleSubmit, formState, control, setValue, trigger } =
+    useForm<IUser>();
+  const [selectedDate, setSelectedDate] = useState<string>('');
+  const [selectedGender, setSelectedGender] = useState<string | null>(null);
+
+  const onSubmit = (data: IUser) => {
+    const newUserData: IUser = {
+      id: lastId + 1,
+      nickname: data.nickname,
+      birthday: formatDateToString(new Date(data.birthday)),
+      sex: data.sex,
+      isDeleted: false,
+    };
+
+    addUserData(newUserData, dispatch);
+    onClose();
+  };
+
+  return (
+    <S.Form onSubmit={handleSubmit(onSubmit)}>
+      <S.CloseButton type="button" onClick={onClose}>
+        <S.StyledImg />
+      </S.CloseButton>
+      <S.Column>
+        <S.InfoOption>
+          <S.UserInfo>
+            <S.Label>{PROFILE_LIST.name}</S.Label>
+            <S.Input
+              {...register('nickname', {
+                required: ADD_USER_FORM.errName,
+              })}
+              type="text"
+              placeholder={ADD_USER_FORM.infoName}
+            />
+          </S.UserInfo>
+          <S.ErrorMessage>{formState.errors.nickname?.message}</S.ErrorMessage>
+        </S.InfoOption>
+        <S.InfoOption>
+          <S.UserInfo>
+            <S.Label>{PROFILE_LIST.birthday}</S.Label>
+            <S.Input
+              placeholder={ADD_USER_FORM.infoData}
+              value={selectedDate}
+              readOnly
+            />
+          </S.UserInfo>
+          <Controller
+            name="birthday"
+            control={control}
+            render={({ field }) => (
+              <CustomCalendar
+                onDateChange={(date: Date) => {
+                  const offset = date.getTimezoneOffset();
+                  const adjustedDate = new Date(
+                    date.getTime() - offset * 60 * 1000,
+                  );
+                  const formattedDate = adjustedDate
+                    .toISOString()
+                    .split('T')[0];
+                  setSelectedDate(formattedDate);
+                  field.onChange(date);
+                }}
+              />
+            )}
+            rules={{ required: ADD_USER_FORM.errData }}
+          />
+          <S.ErrorMessage>{formState.errors.birthday?.message}</S.ErrorMessage>
+        </S.InfoOption>
+        <S.InfoOption>
+          <S.UserInfo>
+            <S.Label>{PROFILE_LIST.sex}</S.Label>
+            <S.GenderButton
+              type="button"
+              id="maleButton"
+              key={'m'}
+              selected={selectedGender === 'm'}
+              onClick={async () => {
+                setSelectedGender('m');
+                setValue('sex', 'm');
+                await trigger('sex');
+              }}
+            />
+            <label htmlFor="maleButton">{SEX_LIST.m}</label>
+            <S.GenderButton
+              type="button"
+              id="femaleButton"
+              key={'w'}
+              selected={selectedGender === 'w'}
+              onClick={async () => {
+                setSelectedGender('w');
+                setValue('sex', 'w');
+                await trigger('sex');
+              }}
+            />
+            <label htmlFor="femaleButton">{SEX_LIST.w}</label>
+            <S.Input
+              id="sex"
+              type="hidden"
+              {...register('sex', {
+                required: ADD_USER_FORM.errSex,
+              })}
+              value={selectedGender || ''}
+            />
+          </S.UserInfo>
+          <S.ErrorMessage>{formState.errors.sex?.message}</S.ErrorMessage>
+        </S.InfoOption>
+      </S.Column>
+      <S.Column>
+        <SubmitButton label={SUBMIT_BUTTON.save} large isActive={true} />
+      </S.Column>
+    </S.Form>
+  );
+};
+
+export default AddUserForm;
---
+++ src/components/AddUserForm/styles.ts
@@ -0,0 +1,129 @@
+import styled from 'styled-components';
+import XImg from '../../assets/XImg.svg';
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  height: 100%;
+  border-radius: 5px;
+  background: ${(props) => props.theme.colors.white};
+  box-shadow: 0px 5px 15px 0px #091f5b33;
+  overflow-y: auto;
+  position: relative;
+
+  &::-webkit-scrollbar {
+    width: 10px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    border-radius: 10px;
+    border: 7px solid ${(props) => props.theme.colors.darkGray};
+  }
+`;
+
+export const Column = styled.div`
+  width: 100%;
+
+  &:nth-child(2) {
+    padding: 10%;
+  }
+
+  &:last-child {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding-bottom: 5%;
+  }
+`;
+
+export const InfoOption = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const UserInfo = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
+  padding: 5px 0;
+
+  &:not(:last-child) {
+    border-bottom: 1px solid ${(props) => props.theme.colors.darkGray};
+  }
+
+  label {
+    font-size: 14px;
+    font-weight: 400;
+    color: ${(props) => props.theme.colors.darkBlue};
+    cursor: pointer;
+  }
+
+  &:last-child {
+    border-bottom: none;
+  }
+`;
+
+export const Label = styled.div`
+  display: flex;
+  width: 80px;
+  font-size: 16px;
+  font-weight: 600;
+  color: ${(props) => props.theme.colors.darkBlue};
+`;
+
+export const ErrorMessage = styled.div`
+  color: ${(props) => props.theme.colors.red};
+  font-size: 14px;
+  font-weight: 400;
+  display: block;
+  margin-bottom: 10px;
+`;
+
+export const Input = styled.input`
+  flex: 1;
+  padding: 5px;
+  border: none;
+  font-size: 16px;
+  font-weight: 400;
+  background-color: ${(props) => (props.value ? 'transparent' : 'initial')};
+  color: ${(props) => props.theme.colors.darkBlue};
+
+  &:focus {
+    outline: none;
+  }
+`;
+
+export const GenderButton = styled.button<{ selected: boolean }>`
+  padding: 5px;
+  background-color: ${(props) =>
+    props.selected ? props.theme.colors.lightBlue : 'transparent'};
+  border: 1px solid ${(props) => props.theme.colors.lightBlue};
+  border-radius: 50%;
+  cursor: pointer;
+  margin: 5px;
+
+  &:focus {
+    outline: none;
+  }
+`;
+
+export const CloseButton = styled.button`
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  background: none;
+  border: none;
+  font-size: 24px;
+  cursor: pointer;
+`;
+
+export const StyledImg = styled(XImg)`
+  position: absolute;
+  top: 10px;
+  right: 5px;
+  cursor: pointer;
+  color: ${(props) => props.theme.colors.darkBlue};
+`;
