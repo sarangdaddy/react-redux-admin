35645bf4d4fc101c1aabf7daa7b67452c12c62a8
sarang_daddy <sarang_daddy@naver.com>
2023-10-28 22:58:22 +0900

    ADD: Added label constants for using in AddUserForm

--- src/constants/label.ts
+++ src/constants/label.ts
@@ -8,3 +8,12 @@ export const SEX_LIST = {
   m: '남자',
   w: '여자',
 };
+
+export const ADD_USER_FORM = {
+  infoName: '입력해주세요.',
+  errName: '닉네임은 공백 없이 영어, 한글로 작성해주세요.',
+  infoData: '아래 달력에서 선택해주세요.',
+  errData: '생년월일을 선택해주세요.',
+  infoSex: '성별을 선택해주세요.',
+  errSex: '성별을 선택해주세요.',
+};
