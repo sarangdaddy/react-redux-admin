73edf7a50ff236b86cafe2d1bdc5b0cb2ee8c0be
sarang_daddy <sarang_daddy@naver.com>
2023-11-27 00:02:05 +0900

    EDIT: Add ProvideisTags and invalidatesTags for cache mangement in RTK Query

--- src/modules/services/users.ts
+++ src/modules/services/users.ts
@@ -6,23 +6,33 @@ const BASE_API_URL = 'http://localhost:9000';
 export const userApi = createApi({
   reducerPath: 'userApi',
   baseQuery: fetchBaseQuery({ baseUrl: BASE_API_URL }),
+  tagTypes: ['Users'],
   endpoints: (builder) => ({
     getUsers: builder.query<IUser[], void>({
       query: () => '/user_data',
+      providesTags: (result) =>
+        result
+          ? [
+              ...result.map(({ id }) => ({ type: 'Users' as const, id })),
+              { type: 'Users', id: 'LIST' },
+            ]
+          : [{ type: 'Users', id: 'LIST' }],
     }),
-    addUser: builder.mutation<IUser, IUser>({
+    addUser: builder.mutation<void, IUser>({
       query: (user) => ({
         url: '/user_data',
         method: 'POST',
         body: user,
       }),
+      invalidatesTags: [{ type: 'Users', id: 'LIST' }],
     }),
-    updateUsers: builder.mutation<IUpdateUsers, IUpdateUsers>({
+    updateUsers: builder.mutation<void, IUpdateUsers>({
       query: ({ ids, updateValue }) => ({
         url: `/user_data`,
         method: 'PATCH',
         body: { ids, isDeleted: updateValue },
       }),
+      invalidatesTags: [{ type: 'Users', id: 'LIST' }],
     }),
   }),
 });
